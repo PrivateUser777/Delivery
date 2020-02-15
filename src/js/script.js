26fcdcea8187443513c812f21dd1b4a8b7f53b5e
PrivateUser <PrivateUser333@mail.ru>
2020-02-15 12:47:12 +0300

    save-6

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,41 @@
 
 new WOW().init();
 
+$(document).ready(function(){
+    //Слайдер
+    $('.reviews__wrappers').slick({
+        speed: 1300,
+        prevArrow: '<button type="button" class="slick-prev"><img src = "../img/Icons/reviewsIconLeft.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src = "../img/Icons/reviewsIconRight.png"></button>'
+    });
+
+    //Модальные окна
+    $('[data-modal = consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay, .consultation').fadeOut('slow');
+    });
+
+    $('[data-modal = button]').on('click', function(){
+        alert('Данный раздел находится в разработке, просим пращение за доставленные неудобства :)');
+    });
+
+    //Плавный переход по ссылкам
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
+        return false;
+    });
+
+    //Появление и скрытие элемента при скроле
+    $(window).scroll(function(){
+        if($(this).scrollTop()>2000){
+            $('.scrollup').fadeIn();
+        } else{
+            $('.scrollup').fadeOut();
+        }
+    });
+});
+
+
